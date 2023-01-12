4e7367f960b5c5cc2f1482b59b969623643b9ef3
Yahya <yahyahussein670@gmail.com>
2023-01-12 09:47:43 +0300

    Add the various relationships and validations in the various models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,14 @@
 class Booking < ApplicationRecord
+    belongs_to :event 
+    belongs_to :user
+    has_many :tickets
+
+    validates :quantity, presence: true, numericality: { greater_than: 0 }
+
+    private
+
+    def calculate_total_price
+      self.total = event.price * quantity
+    end
+  
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+    has_many :bookings
+    has_many :tickets, through: :bookings
+    belongs_to :user
+
+    validates :title, :date, :location, :price, presence: true
 end
