9b1cda13c8248b401b91f37c4e05b20c6bee4c53
Yahya <yahyahussein670@gmail.com>
2023-02-05 12:47:33 +0300

    update the event relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,20 @@ class Event < ApplicationRecord
     has_many :bookings
     has_many :tickets, through: :bookings
     belongs_to :user
+    belongs_to :location
 
-    validates :title, :date, :location, :price, presence: true
+    attribute :date, :datetime
+
+    CATEGORIES = ["Music", "Entertainment", "Business", "Holiday", "Sports and Fitness, Food and Drinks", "Tech meetups" ]
+
+    validates :category, inclusion: {
+        in: CATEGORIES,
+        message: "must be one of the following: #{CATEGORIES.join(',')}"
+    }
+
+    validates :title, :date, presence: true
+
+    def self.by_category(category)
+        where(category: category)
+    end
 end
