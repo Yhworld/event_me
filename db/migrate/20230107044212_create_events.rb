6686a8f4fd554dd3314035586557b5da923609f5
Yahya <yahyahussein670@gmail.com>
2023-02-13 07:31:51 +0300

    update the event table and the location table

--- db/migrate/20230107044212_create_events.rb
+++ db/migrate/20230107044212_create_events.rb
@@ -2,13 +2,13 @@ class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
       t.string :title
-      t.date :date
+      t.datetime :date
+      t.string :category
       t.string :description
       t.string :about
       t.string :image
-      t.string :location
       t.integer :user_id
-      t.time :time
+      t.integer :location_id
 
       t.timestamps
     end
