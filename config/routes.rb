c279a5415c71760509e23ed6aee452c96ea4aced
Yahya <yahyahussein670@gmail.com>
2023-03-11 06:43:29 +0300

    update the seed and route file

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,18 @@ Rails.application.routes.draw do
   resources :locations
   resources :tickets
   resources :bookings
-  # resources :events
+  resources :events
   resources :users
 
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
+  patch "/updatepost/:id", to: "events#update"
   # get '/events/category/:category', to: 'events#index'
-  get '/all/:category', to: 'events#index'
+  get '/all/:category', to: 'events#category'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'locations', to: 'locations#index'
+  get 'location/:place', to: 'locations#by_location'
+  get 'locationss/:place/:category', to: 'locations#filter_by_location_and_category'
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 event1 = Event.create(title: "My Event", description: "A description of my event.", category: "Music", date: "2023-01-30 14:00:00", about: "online", image: "jjjjj", user_id: "10", location_id: "1")
 event2 = Event.create(title: "testrun", description: "A description of my event.", category: "Holiday", date: "2023-01-30 14:00:00", about: "online", image: "jjjjj", user_id: "10", location_id: "3")
-
+event3 = Event.create(title: "testrun2", description: "A description of my event.", category: "Food", date: "2023-03-30 14:00:00", about: "online", image: "https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg", user_id: "10", location_id: "2")
+event4 = Event.create(title: "testrun3", description: "A description of my event.", category: "Health", date: "2023-03-30 14:00:00", about: "online", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTARoeMoKCKlBx1_9PJ2ujDAPn5WbUeCcaYKA&usqp=CAU", user_id: "10", location_id: "3")
 
 # location = Location.create
