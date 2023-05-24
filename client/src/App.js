3dd54a177893285e2facb2d6b3520d489edf3e6c
Yahya <yahyahussein670@gmail.com>
2023-05-24 15:46:09 +0300

    App js mainfile

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
-
+import CategoryList from "./component/CategoryList";
+import Events from "./component/Events";
+import Hero from './component/Hero';
+import SearchLocation from "./component/SearchLocation";
+// import Login from './component/Login';
+// import Register from './component/Register';
+import Switch from './component/Switch';
+import Titlebar from './component/Titlebar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Titlebar />
+    <Hero />    
+    <CategoryList />
+    <SearchLocation />
+    <Events />
+    </>
   );
 }
 
